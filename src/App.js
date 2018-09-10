0509faac843d8f26731b60fe188c0bb967a4fd19
DarkYamato <werewolfoftheshadow@gmail.com>
2018-09-10 17:10:48 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -6,49 +6,89 @@ class App extends Component {
   state = {
     name: '',
     breed: '',
-    cards: ''
+    nickname: '',
+    cards: []
   }
 
-  handleChangeName = (e) => {
-    this.setState({
-      name: e.target.value,
-    })
+  componentDidMount(){
+    this.getCards();
   }
 
-  handleChangeBreed = (e) => {
-    this.setState({
-      breed: e.target.value,
-    })
+  handleChange = (name) => e => {
+    this.setState({ 
+        [name]: e.target.value 
+    });
   }
 
   handleSubmit = () => {
-    const { name, breed } = this.state;
+    const { name, breed, nickname } = this.state;
 
     fetch('http://localhost:3001/cards', {
       method: 'post',
       headers: {  
         "Content-Type": "application/json; charset=utf-8" 
       },
-      body:JSON.stringify({ name, breed })
-    });
+      body:JSON.stringify({ name, breed, nickname })
+    })
+    .then(this.setState({
+      name: '',
+      breed: '',
+      nickname: ''
+    }))
+    .then(() => this.getCards());
+    
   }
 
   getCards = () => {
-    fetch('http://localhost:3001/cards').then(res => this.setState({
+    fetch('http://localhost:3001/cards')
+    .then(res => res.json())
+    .then(res => this.setState({
       cards: res
     }));
+    
+  }
+
+  deleteCard = (id) => {
+    fetch(`http://localhost:3001/cards/${id}`, {
+        method: 'delete'
+    })
+    .then(() => this.getCards());
+}
+
+  editCard = (card) => {
+    fetch(`http://localhost:3001/cards/${card.id}`, {
+        method: 'put',
+        headers: {  
+          "Content-Type": "application/json; charset=utf-8" 
+        },
+        body:JSON.stringify({ name: card.name, breed: card.breed, nickname: card.nickname })
+      })
+    .then(() => this.getCards());
   }
 
   render() {
     return (
       <div className="App">
         <div className="add-card">
-          <div className="add-card__title">Добавьте кота</div>
-          <span>Имя: </span><input onChange={this.handleChangeName} value={this.state.name} className="add-card__input" type="text"/>
-          <span>Порода: </span><input onChange={this.handleChangeBreed} value={this.state.breed} className="add-card__input" type="text"/>
-          <button onClick={this.handleSubmit} className="add-card__button">Сохранить</button>
+          <div className="add-card-title">Add an agent (cat)</div>
+          <div className="add-card-inputs">
+            <div className="input-block">
+              <span>Name: </span><input onChange={this.handleChange('name')} value={this.state.name} className="add-card-input" type="text"/>
+            </div>
+            <div className="input-block">  
+              <span>Breed: </span><input onChange={this.handleChange('breed')} value={this.state.breed} className="add-card-input" type="text"/>
+            </div>
+            <div className="input-block">
+              <span>Nickname: </span><input onChange={this.handleChange('nickname')} value={this.state.nickname} className="add-card-input" type="text"/>
+            </div>         
+          </div>
+          <button onClick={this.handleSubmit} className="add-card-button">Add to base</button>
         </div>
-        <Card/>
+        <div className="items">
+          {this.state.cards.map((x,i) => 
+            <Card name={x.name} breed={x.breed} nickname={x.nickname} id={i+1} deleteCard={()=> this.deleteCard(x.id)} editCard={this.editCard} key={x.id} />
+          )}
+        </div>
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
+import './Card.css';
 
 class Card extends Component {
-  render() {
+    state = {
+        disabled: true,
+        name: this.props.name,
+        breed: this.props.breed,
+        nickname: this.props.nickname
+    }   
+
+    handleEdit = (e) => {
+        this.setState({
+            disabled: false
+        });
+    }
+
+    handleSave = () => {
+        this.setState({
+            disabled: true
+        });
+        return {
+                name: this.state.name, 
+                breed: this.state.breed, 
+                nickname: this.state.nickname,
+                id: this.props.key
+               }
+    }
+
+    handleChange = (name) => e => {
+        this.setState({ 
+            [name]: e.target.value 
+        });
+      }
+
+    render() {
     return (
-      <div className="card">
-        <div className="card_name"></div>
-        <div className="card_breed"></div>
-      </div>
+    <div className="card">
+        <div className="card-header">
+            <div className="card-header-buttons">
+                <button onClick={this.props.deleteCard} className="delete-button"><ion-icon name="close"></ion-icon></button>
+                {!this.state.disabled ? 
+                <button onClick={() => this.props.editCard(this.handleSave())} className="save-button"><ion-icon name="save"></ion-icon></button>
+                    :
+                <button onClick={this.handleEdit} className="edit-button"><ion-icon name="create"></ion-icon></button>
+                }
+            </div>
+            <div className="card-id">Agent's file #<span>{this.props.id}</span></div>
+        </div>
+        <div className="card-item-container">Name: <input onChange={this.handleChange('name')} className="card-item" value={this.state.name} disabled={this.state.disabled}/></div>
+        <div className="card-item-container">Breed: <input onChange={this.handleChange('breed')} className="card-item" value={this.state.breed} disabled={this.state.disabled}/></div>
+        <div className="card-item-container">Nickname: <input onChange={this.handleChange('nickname')} className="card-item" value={this.state.nickname} disabled={this.state.disabled}/></div>
+    </div>
     );
   }
 }
